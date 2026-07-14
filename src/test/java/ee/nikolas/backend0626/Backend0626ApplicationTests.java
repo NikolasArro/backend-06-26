b27d26e78e2cdb3b4f10413e42db150eed37a3d3
Nikolas Arro <arronikolas@gmail.com>
2026-07-14 17:30:37 +0300

    Tests

---
+++ src/test/java/ee/nikolas/backend0626/Backend0626ApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.nikolas.backend0626;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend0626ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
